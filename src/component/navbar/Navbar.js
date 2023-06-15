f1392995240af0562448eac00d82c7aa98efacd7
Hitesh <hp5741609@gmail.com>
2023-06-15 18:21:27 +0530

    Added Link tag for SignUp

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -173,13 +173,13 @@ export default function Navbar() {
                   </div> */}
 
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don’t have an account yet?{" "}
-                    <a
-                      href="/"
+                    Don't have an account yet?{' '}
+                    <Link
+                      to="/signup"
                       className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                     >
-                      Sign up
-                    </a>
+                    Sign up
+                    </Link>
                   </p>
                 </form>
               </div>
